d631801c69d3cd208c9a00ef7ce3fbf75191be13
Junfeng Zhange <jeff@tenbits.io>
2025-10-06 20:30:28 +0800

    Add missing license header
    ### Motivation
    
    After combine two framework corelibs and essentials, some files are missing license header.
    
    ### Modifications
    
    - Fix missing license header.
    
    ### Result
    
    License header check passed.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,18 @@
 // swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Netbot open source project
+//
+// Copyright (c) 2025 Junfeng Zhang and the Netbot project authors
+// Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Netbot project authors
 //
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import CompilerPluginSupport
 import PackageDescription
--- Sources/_ProfileSupport/Compatibility/RangeOf.swift
+++ Sources/_ProfileSupport/Compatibility/RangeOf.swift
@@ -1,6 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Netbot open source project
+//
+// Copyright (c) 2025 Junfeng Zhang and the Netbot project authors
+// Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Netbot project authors
 //
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 #if canImport(FoundationEssentials)
   @available(SwiftStdlib 5.3, *)
