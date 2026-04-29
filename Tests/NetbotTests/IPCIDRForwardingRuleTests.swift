7e23bdeb1645339b2ff089ddff4ad253f0bad1f3
Junfeng Zhange <jeff@tenbits.io>
2026-04-30 02:07:22 +0800

    Add IPv6 pool
    Motivation:
    
    This pool is used in IP-CIDR forwarding rule.
    
    Modifications:
    
    - Add new AvailableIPv6Pool.
    - Update IPCIDRForwardingRule to use new AvailableIPv6Pool.
    
    Result:
    
    AvailableIPv6Pool is available from SwiftStdlib 6.0.

--- Tests/NetbotTests/IPCIDRForwardingRuleTests.swift
+++ Tests/NetbotTests/IPCIDRForwardingRuleTests.swift
@@ -28,62 +28,6 @@ struct IPCIDRForwardingRuleTests {
   #else
     @available(SwiftStdlib 6.0, *)
   #endif
-  @Test func createAddressesWithIPCIDRString() async throws {
-    let testVectors = [
-      (
-        "2001:4860:4860::8888/32",
-        ("2001:4860:0000:0000:0000:0000:0000:0000", "2001:4860:ffff:ffff:ffff:ffff:ffff:ffff")
-      ),
-      (
-        "2001:4860:4860::8888/0",
-        ("0000:0000:0000:0000:0000:0000:0000:0000", "FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF")
-      ),
-      (
-        "2001:4860:4860::8888/128",
-        ("2001:4860:4860::8888", "2001:4860:4860::8888")
-      ),
-    ]
-
-    for (address, (lowerBound, upperBound)) in testVectors {
-      let l = try #require(
-        IPCIDRForwardingRule.AvailableIPv6Pool(uncheckedBounds: address)?.lower)
-      var expected = try SocketAddress(ipAddress: lowerBound, port: 0)
-      #expect(SocketAddress.v6(l) == expected)
-
-      let u = try #require(
-        IPCIDRForwardingRule.AvailableIPv6Pool(uncheckedBounds: address)?.upper)
-      expected = try SocketAddress(ipAddress: upperBound, port: 0)
-      #expect(SocketAddress.v6(u) == expected)
-    }
-  }
-
-  #if NETBOT_REQUIRES_SUPPORT_EARLY_OS_VERSIONS
-    @available(SwiftStdlib 5.3, *)
-  #else
-    @available(SwiftStdlib 6.0, *)
-  #endif
-  @Test(arguments: [
-    "192.168/21",
-    "192.168.0.1",
-    "192.168.0.1/33",
-    "192.168.0.1/",
-    "192.168.0.1/x",
-    "/var/tmp/20",
-    "var/20",
-    "2001:4860:4860::8888",
-    "2001:4860:4860::8888/129",
-    "2001:4860:4860::8888/",
-    "2001:4860:4860::8888/x",
-  ])
-  func createAddressesWithInvalidIPCIDRString(_ cidr: String) async throws {
-    #expect(IPCIDRForwardingRule.AvailableIPv6Pool(uncheckedBounds: cidr) == nil)
-  }
-
-  #if NETBOT_REQUIRES_SUPPORT_EARLY_OS_VERSIONS
-    @available(SwiftStdlib 5.3, *)
-  #else
-    @available(SwiftStdlib 6.0, *)
-  #endif
   @Test func propertyInitialValue() async throws {
     let forwardingRule = FINALForwardingRule("test", forwardProtocol: .direct)
     #expect(forwardingRule.condition == "test")
